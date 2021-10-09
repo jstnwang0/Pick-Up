4703aef192b5f9546607720dc00af9ab24587d15
Justin <protolate@outlook.com>
2021-10-08 20:13:40 -0700

    added ability to add contacts on new chat page

--- app/screens/chatScreens/NewMessage.js
+++ app/screens/chatScreens/NewMessage.js
@@ -1,13 +1,28 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  ScrollView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import BackButton from "../../components/BackButton";
 import { FontTextBold, FontText } from "../../components/FontText";
 import SearchBar from "../../components/SearchBar";
 import colors from "../../config/colors";
 
 export default function NewMessage({ navigation }) {
+  const [contacts, setContacts] = useState([
+    { name: "Justin", selected: false },
+    { name: "Jerry", selected: false },
+  ]);
+
+  const [selected, setSelected] = useState([]);
+
   return (
-    <View style={styles.container}>
+    <View style={{ backgroundColor: "white" }}>
       <View style={styles.topBar}>
         <View>
           <BackButton navigation={navigation} />
@@ -18,17 +33,107 @@ export default function NewMessage({ navigation }) {
           </View>
           <View>
             <FontText style={{ color: colors.mediumGray }}>
-              4 of 200 selected
+              {selected.length} of {contacts.length} selected
             </FontText>
           </View>
         </View>
       </View>
-      <View style={{ width: "100%", marginTop: 15, alignItems: "center" }}>
+      {selected.length != 0 ? (
+        <View>
+          <View style={{ marginTop: 15, marginLeft: 20 }}>
+            <FontTextBold style={{ fontSize: 18 }}>To</FontTextBold>
+            <ScrollView
+              style={{ marginTop: 5 }}
+              horizontal={true}
+              bounces={false}
+              showsHorizontalScrollIndicator={false}
+            >
+              {selected.map((contact, index) => (
+                <View key={index}>
+                  <TouchableOpacity
+                    activeOpacity={0.5}
+                    onPress={() => {
+                      const newContacts = [...contacts];
+                      const contactsIndex = contacts.indexOf(contact);
+                      newContacts[contactsIndex].selected =
+                        !contacts[contactsIndex].selected;
+                      setContacts(newContacts);
+
+                      const newSelected = [...selected];
+                      newSelected.splice(newSelected.indexOf(contact), 1);
+
+                      setSelected(newSelected);
+                    }}
+                    style={{
+                      backgroundColor: colors.darkGreen,
+                      paddingLeft: 15,
+                      paddingRight: 10,
+                      height: 40,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      borderRadius: 20,
+                      marginRight: 10,
+                      flexDirection: "row",
+                    }}
+                  >
+                    <FontText style={{ color: "white", marginRight: 5 }}>
+                      {contact.name}
+                    </FontText>
+                    <Image
+                      source={require("../../assets/closeIcon.png")}
+                      style={{ height: 20, width: 20, tintColor: "white" }}
+                    />
+                  </TouchableOpacity>
+                </View>
+              ))}
+            </ScrollView>
+          </View>
+        </View>
+      ) : (
+        <View></View>
+      )}
+
+      <View
+        style={{
+          width: "100%",
+          marginTop: 15,
+          paddingBottom: 15,
+          alignItems: "center",
+        }}
+      >
         <SearchBar />
       </View>
-      <View style={{ width: "90%", marginTop: 20 }}>
-        <FontText style={{ fontSize: 17 }}>Suggested</FontText>
-      </View>
+      <ScrollView style={{ width: "100%" }}>
+        {contacts.map((contact, index) => (
+          <TouchableWithoutFeedback
+            key={index}
+            onPress={() => {
+              const newContacts = [...contacts];
+              newContacts[index].selected = !contacts[index].selected;
+              setContacts(newContacts);
+
+              const newSelected = [...selected];
+              if (newSelected.includes(contact)) {
+                newSelected.splice(newSelected.indexOf(contact), 1);
+              } else {
+                newSelected.push(contact);
+              }
+
+              setSelected(newSelected);
+            }}
+          >
+            <View
+              style={{
+                width: "100%",
+                height: 75,
+                backgroundColor: contact.selected ? "green" : "red",
+              }}
+            >
+              <FontText style={{ fontSize: 17 }}>{contact.name}</FontText>
+            </View>
+          </TouchableWithoutFeedback>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -37,14 +142,8 @@ const styles = StyleSheet.create({
   topBar: {
     width: "100%",
     marginTop: 60,
-    marginLeft: 50,
+    marginLeft: 20,
     flexDirection: "row",
     alignItems: "center",
-  },
-  container: {
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    backgroundColor: "white",
   },
 });
